936c486320fa5a23088a6c29fc0f50145f8470d3
Anh Le Hoang <lehoanganh25991@gmail.com>
2019-05-27 16:04:28 +0700

    [ha] Best for ever

--- test-high-availability/moleculer.config.js
+++ test-high-availability/moleculer.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   nodeID: 'node-' + new Date().getTime(),
   transporter: 'nats://localhost:4222',
+  retryPolicy: {
+    enabled: true,
+    retries: 5,
+    delay: 100,
+    maxDelay: 2000,
+    factor: 2,
+    check: err => err && !!err.retryable,
+  },
 };
